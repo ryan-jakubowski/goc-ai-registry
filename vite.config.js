ca6821abca6533f501e8e4bcd4cf4b4574cb6ba9
Ryan Jakubowski <ryanjakubowski@Ryans-MacBook-Air.local>
2025-11-30 14:44:33 -0500

    Update base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: './', // For GitHub Pages relative paths
+    base: '/goc-ai-registry/', // For GitHub Pages
 })
